da1d1d56bc0e3b10e662e2aa21513fb9362aca59
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-09-25 09:08:05 -0300

    :sparkles: Primeira versão

---
+++ ExemploPopUp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace ExemploPopUp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var page = new PopUpPage();
+
+            await PopupNavigation.Instance.PushAsync(page);
+        }
+    }
+}
---
+++ ExemploPopUp/PopUpPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace ExemploPopUp
+{
+    public partial class PopUpPage : PopupPage
+    {
+        public PopUpPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        protected override async Task OnAppearingAnimationEndAsync()
+        {
+           
+        }
+
+        protected override async Task OnDisappearingAnimationBeginAsync()
+        {
+           
+        }
+    }
+}
